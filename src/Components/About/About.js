90f6fc8cd8bbcafee794892c9c73a79d2e4be366
sugu-mar <sugumargpt@gmail.com>
2024-01-09 11:43:49 +0530

    navbar completed

---
+++ src/Components/About/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Element } from 'react-scroll';
+import './About.css'
+
+const About = () => {
+  return (
+    <Element name="about" className='about'>
+        <p>lorem100jdchkjshxksjhcxnwkdhcknsdjhbchjbxnmsnd gdhfhg d wcchh sc ihdh sidf  df</p>
+        <h1>fsdfij</h1>
+        <h3>chfmefj</h3>
+        <h3>kjbxjkbjf</h3>
+    </Element>
+  )
+}
+
+export default About;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import './Navbar.css';
+import {Link} from 'react-scroll';
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+      <div className='navbar__left'>
+        <h1>Develop<span>er</span></h1>
+      </div>
+        <div className='navbar__right'>
+          <Link 
+            to='about'  
+            smooth={true} 
+            duration={500}
+            ><h4>About me</h4>
+          </Link>
+          <Link 
+            to='skills'  
+            smooth={true} 
+            duration={500}
+            ><h4>Skills</h4>
+          </Link>
+          <Link 
+            to='projects'  
+            smooth={true} 
+            duration={500}
+            ><h4>Projects</h4>
+          </Link>
+          <Link 
+            to='education'  
+            smooth={true} 
+            duration={500}
+            ><h4>Education</h4>
+          </Link>
+          <Link 
+            to='contact'  
+            smooth={true} 
+            duration={500}
+            ><h4>Contact</h4>
+          </Link>
+
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
