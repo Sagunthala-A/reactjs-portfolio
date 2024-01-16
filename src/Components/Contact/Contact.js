cb980cd514e94f3c237927a54f43e7dab67dd68d
Sagunthala-A <sagunthala9925@gmail.com>
2024-01-16 09:58:58 +0530

    ..

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -2,19 +2,22 @@ import React from 'react';
 import { Element } from 'react-scroll';
 import './Contact.css';
 import { AccessAlarm,GitHub,LinkedIn,Mail } from '@mui/icons-material';
-// import GitHubIcon from '@mui/icons-material/GitHub';
 function Contact() {
+
+  const handleMailtoClick = () => {
+    window.location.href = 'mailto:sagunthala9925@gmail.com';
+  };
   return (
     <Element name='contact' className='contact'>
           <h1>Contact</h1>
           <div className='contact__icons'>
-            <a href='#' target='_blank'>
+            <a href='mailto:sagunthala9925@gmail.com' target='_blank'>
               <Mail className='icons'/>
             </a>
-            <a href='#' target='_blank'>
+            <a href='www.github.com' target='_blank'>
               <GitHub/>
             </a>
-            <a href='#' target='_blank'>
+            <a href='www.linkedin.com' target='_blank'>
               <LinkedIn/>
             </a>
         </div>
--- src/Components/HireMe/HireMe.js
+++ src/Components/HireMe/HireMe.js
@@ -4,13 +4,20 @@ import {Link} from 'react-router-dom'
 
 const HireMe = () => {
   return (
-    <Link to='/hireme' className='hireme'>
-        heeellll
-            <h1>
-                hello world
-            </h1>
+    <Link to='/hireme' className='hireContainer'>
+      <div className='hireContainer'>
+        <a href='mailto:sagunthala9925@gmail.com' target='_blank'>Hiring </a>
+        <a href="mailto:no-one@snai1mai1.com?subject=free chocolate">example</a>
+
+        <a href="mailto:manish@simplygraphix.com?subject=Feedback for 
+webdevelopersnotes.com&body=The Tips and Tricks section is great
+&cc=anotheremailaddress@anotherdomain.com
+&bcc=onemore@anotherdomain.com">Send me an email</a>
+
+      </div>
+
     </Link>
   )
 }
 
-export default HireMe
+export default HireMe;
