2f74563d8fd2b8632edcf9ed560cd410d4e49176
Sagunthala-A <sagunthala9925@gmail.com>
2024-03-08 17:30:51 +0530

    adding book navigator app

--- src/Components/ProjectContainer/ProjectContiner.js
+++ src/Components/ProjectContainer/ProjectContiner.js
@@ -3,6 +3,7 @@ import { Element } from 'react-scroll';
 import './ProjectContainer.css';
 import Project from '../Project/Project';
 import financeApp from '../../assets/Finance-tracker-app.png';
+import bookNavigatorApp from '../../assets/Book-Navigator-app.png';
 import img2 from "../../assets/jojo/project11.png";
 import img3 from "../../assets/jojo/project12.png";
 import img4 from "../../assets/jojo/project13.png";
@@ -13,11 +14,12 @@ import img6 from "../../assets/jojo/project15.png";
 const ProjectConatiner = () => {
   const projectList = [
       {
-        img: financeApp,
-        title: "Finance Tracker App",
-        desc:"It is used to manage the expensive and income of a individual person.",
-        link: "https://sagu-finance-tracker-app.vercel.app/",
-        gitLink: "https://github.com/Sagunthala-A/Finance-Tracker-App"
+        img: bookNavigatorApp,
+        title: "Book Navigator App",
+        desc:"Explore millions of books and find your next read with Book Navigator (powered by Google Books).",
+        // desc:"Dive into the world of books and discover your next great read with Book Navigator App!"
+        link: "https://virtual-book-beta.vercel.app/",
+        gitLink: "https://github.com/Sagunthala-A/VirtualBook"
       },
       {
         img: financeApp,
