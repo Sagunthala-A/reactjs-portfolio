dc7d89cd85b3902ee5f6148e2b3afe6e79dae277
Sagunthala-A <sagunthala9925@gmail.com>
2024-06-18 22:24:19 +0530

    added new projects

--- src/Components/Context/DataProvider.js
+++ src/Components/Context/DataProvider.js
@@ -2,6 +2,9 @@ import DataContext from "./DataContext";
 import { useEffect, useState } from "react";
 import financeApp from '../../assets/Finance-tracker-app.png';
 import bookNavigatorApp from '../../assets/Book-Navigator-app.png';
+import cryptoTrackerApp from '../../assets/crypto-tracker-app.png';
+import toDoApp from '../../assets/to-do-app.png';
+import portfolioApp from '../../assets/portfolio-app.png';
 import comingSoon from '../../assets/coming-soon.jpg';
 // import img2 from "../../assets/jojo/project11.png";
 // import img3 from "../../assets/jojo/project12.png";
@@ -15,43 +18,49 @@ const DataProvider = ({children})=>{
     const [toggleMenuBar,setToggleMenuBar] = useState(false);
     const projectList = [
       {
-        img: bookNavigatorApp,
-        title: "Book Navigator App",
-        desc:"Explore millions of books and find your next read with Book Navigator (powered by Google Books).",
-        // desc:"Dive into the world of books and discover your next great read with Book Navigator App!"
-        link: "https://virtual-book-beta.vercel.app/",
-        gitLink: "https://github.com/Sagunthala-A/VirtualBook"
-      },
-      {
         img: financeApp,
         title: "Finance Tracker App",
-        desc:"Track your income & expenses effortlessly. Take control of your finances today!  (This project is ongoing.)",
+        desc: "Track your income & expenses effortlessly. Take control of your finances today!",
         link: "https://sagu-finance-tracker-app.vercel.app/",
-        gitLink: "https://github.com/Sagunthala-A/Finance-Tracker-App"
+        gitLink: "https://github.com/Sagunthala-A/Finance-Tracker-App",
       },
       {
-        img: comingSoon,
+        img: cryptoTrackerApp,
+        title: "Crypto Tracker App",
+        desc: "Stay on top of the ever-changing cryptocurrency market. Get live prices, market data, insightful charts, and analyze trends – all in one place",
+        link: "https://sagu-crypto-tracker.vercel.app/",
+        gitLink: "https://github.com/Sagunthala-A/crypto-tracker",
+      },
+      {
+        img: toDoApp,
         title: "ToDo List App",
-        desc:"Organize your day and boost your productivity with the Todo List App. Create, manage, and track your tasks efficiently!",
+        desc: "Organize your day and boost your productivity with the Todo List App. Create, manage, and track your tasks efficiently!",
         link: "https://react-to-do-app-flame.vercel.app/",
-        gitLink: "https://github.com/Sagunthala-A"
+        gitLink: "https://github.com/Sagunthala-A/to-do-app",
       },
       {
-        img: comingSoon,
-        title: "Coming Soon",
-        desc:"Project will be launch soon",
-        link: "https://sagu-finance-tracker-app.vercel.app/",
-        gitLink: "https://github.com/Sagunthala-A"
+        img: bookNavigatorApp,
+        title: "Book Navigator App",
+        desc: "Explore millions of books and find your next read with Book Navigator (powered by Google Books).",
+        // desc:"Dive into the world of books and discover your next great read with Book Navigator App!"
+        link: "https://virtual-book-beta.vercel.app/",
+        gitLink: "https://github.com/Sagunthala-A/VirtualBook",
       },
       {
-        img: comingSoon,
-        title: "Coming Soon",
-        desc:"Project will be launch soon",
-        link: "https://sagu-finance-tracker-app.vercel.app/",
-        gitLink: "https://github.com/Sagunthala-A"
+        img: portfolioApp,
+        title: "Sagunthala's Portfolio",
+        desc: "Evolving web developer, always learning, always growing. Explore my portfolio!.",
+        link: "https://sagunthala-a-portfolio.vercel.app/",
+        gitLink: "https://github.com/Sagunthala-A/reactjs-portfolio",
       },
-
-    ]
+      // {
+      //   img: comingSoon,
+      //   title: "Coming Soon",
+      //   desc: "Project will be launch soon",
+      //   link: "www.google.com",
+      //   gitLink: "https://github.com/Sagunthala-A",
+      // },
+    ];
     const [hovered, setHovered] = useState(false);
     useEffect(() => {
     // Update body background color when isDarkMode changes
