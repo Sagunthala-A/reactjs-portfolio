29065188d57196f75069b24921cf2a8314a7af4c
Sagunthala-A <sagunthala9925@gmail.com>
2024-06-19 17:55:00 +0530

    replaced a old resume

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,21 +4,32 @@ import {Link} from 'react-scroll'
 
 const About = () => {
   return (
-    <div className='about'>
-      <div className='about__container'>
+    <div className="about">
+      <div className="about__container">
         <h1>Ms.Sagunthala</h1>
-        <p>"An enthusiastic frontend developer eagerly waiting to showcase my skills and broaden my knowledge."</p>
-        <div className='about__buttons'>
-          <a href="https://drive.google.com/drive/u/2/folders/1h5rP07gAEcUUu_vQ9htsbJRwEQnHplkx" target="_blank" rel="noopener noreferrer">
-            <button className='about__downloadResumeBtn'>Download Resume</button>
+        <p>
+          "An enthusiastic frontend developer eagerly waiting to showcase my
+          skills and broaden my knowledge."
+        </p>
+        <div className="about__buttons">
+          {/* https://drive.google.com/file/d/1NFPGotthE_6q_pvoYQRsEgadq32Ah_N0/view */}
+          <a
+            // href="https://drive.google.com/drive/u/2/folders/1h5rP07gAEcUUu_vQ9htsbJRwEQnHplkx"
+            href="https://drive.google.com/file/d/1pdcNiN-oJ_Nu-1GiMPkfu05QYzFjqr6w/view"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className="about__downloadResumeBtn">
+              Download Resume
+            </button>
           </a>
-          <Link to='projects' smooth={true} duration={500}>
-            <button className='about__myWorkBtn'>My work</button>
+          <Link to="projects" smooth={true} duration={500}>
+            <button className="about__myWorkBtn">My work</button>
           </Link>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default About
